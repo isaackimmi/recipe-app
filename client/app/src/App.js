7c30ce50418cd44f966412fdce807165be33229e
Isaac Kim <isaacmmikim@gmail.com>
2022-12-03 17:57:21 -0800

    frontend change.

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,114 +1,158 @@
-import React, {useState} from "react";
-import './App.css';
-import Axios from 'axios'
+import React, { useState } from "react";
+import "./App.css";
+import Axios from "axios";
+import logo from "./recipe-database.png";
 
 function App() {
+  const [Rating, setRating] = useState("");
+  const [Title, setTitle] = useState("");
+  const [Description, setDescription] = useState("");
 
-  const [Rating,setRating] = useState('')
-  const [Title,setTitle] = useState('')
-  const [Description,setDescription] = useState('')
-  
-  const [Total_Time,setTime] = useState('')
-  const [Publish_Date,setDate] = useState('')
-  const [Cuisine,setCusine] = useState('')
+  const [Total_Time, setTime] = useState("");
+  const [Publish_Date, setDate] = useState("");
+  const [Cuisine, setCusine] = useState("");
 
-  const[Author_Name,setAuthor] = useState('')
+  const [Author_Name, setAuthor] = useState("");
 
-
-  const [Preparation,setPreparation] = useState('')
-  const [Measurement,setMeasurement] = useState('')
-  const [Instruction,setInstruction] = useState('')
-  const [Ingredient, setIngredient] = useState('')
-
- 
+  const [Preparation, setPreparation] = useState("");
+  const [Measurement, setMeasurement] = useState("");
+  const [Instruction, setInstruction] = useState("");
+  const [Ingredient, setIngredient] = useState("");
 
   const submitRecipe = () => {
-    Axios.post('http://localhost:3001/api/insert',
-    {
+    Axios.post("http://localhost:3001/api/insert", {
       Rating: Rating,
       Title: Title,
       Description: Description,
       Total_Time: Total_Time,
       Publish_Date: Publish_Date,
       Cuisine: Cuisine,
-      Author_Name: Author_Name
+      Author_Name: Author_Name,
     }).then(() => {
-      alert('sucessfuly insert');
-    })
-  }
-
-
- 
+      alert("sucessfuly insert");
+    });
+  };
 
   return (
-    <div>
-          <h1>Recipe Database</h1>
-          <div className = "form">
-          
-         
-
-          <label>Title</label>
-          <input type = "text" name ="title" onChange = {(e)=> {
-            setTitle(e.target.value)
-          }}/>
-
-          <label>Description</label>
-          <input type = "text" name ="description" onChange = {(e)=> {
-            setDescription(e.target.value)
-          }}/>
-
-          <label>Rating</label>
-          <input type = "text" name = "Rating" onChange={(e) => {
-            setRating(e.target.value)
-          }}/>
-
-          <label>Total Time</label>
-          <input type = "text" name = "total_time" onChange = {(e) => {
-            setTime(e.target.value)
-          }}/>
-
-          <label>Publish Date</label>
-          <input type = "text" name = "publish_date" onChange = {(e) => {
-            setDate(e.target.value)
-          }}/>
-
-          <label>Cuisine</label>
-          <input type = "text" name = "cuisine" onChange = {(e) => {
-            setCusine(e.target.value)
-          }}/>
-
+    <div className="page">
+      <div className="header">
+        <img width={"50px"} height={"50px"} alt="logo" src={logo} />
+        <h1>Recipe Database</h1>
+      </div>
+      <div className="form-container">
+        <div className="form">
           <label>Author Name</label>
-          <input type = "text" name = "Author" onChange = {(e) => {
-            setAuthor(e.target.value)
-          }}/>
+          <input
+            type="text"
+            name="Author"
+            onChange={(e) => {
+              setAuthor(e.target.value);
+            }}
+          />
+        </div>
 
+        <div className="form">
+          <label>Title</label>
+          <input
+            type="text"
+            name="title"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
 
+          <label className="pt-3">Description</label>
+          <input
+            type="text"
+            name="description"
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+
+          <label className="pt-3">Rating</label>
+          <input
+            type="text"
+            name="Rating"
+            onChange={(e) => {
+              setRating(e.target.value);
+            }}
+          />
+
+          <label className="pt-3">Total Time</label>
+          <input
+            type="text"
+            name="total_time"
+            onChange={(e) => {
+              setTime(e.target.value);
+            }}
+          />
+
+          <label className="pt-3">Publish Date</label>
+          <input
+            type="text"
+            name="publish_date"
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+
+          <label className="pt-3">Cuisine</label>
+          <input
+            type="text"
+            name="cuisine"
+            onChange={(e) => {
+              setCusine(e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="form">
           <label>Preparation</label>
-          <input type = "text" name = "preparation" onChange = {(e) => {
-            setPreparation(e.target.value)
-          }}/>
+          <input
+            type="text"
+            name="preparation"
+            onChange={(e) => {
+              setPreparation(e.target.value);
+            }}
+          />
 
-          <label>Measurement</label>
-          <input type = "text" name = "measurement" onChange = {(e) => {
-            setMeasurement(e.target.value)
-          }}/>
+          <label className="pt-3">Measurement</label>
+          <input
+            type="text"
+            name="measurement"
+            onChange={(e) => {
+              setMeasurement(e.target.value);
+            }}
+          />
 
-          <label>Instruction</label>
-          <input type = "text" name = "instruction" onChange = {(e) => {
-            setInstruction(e.target.value)
-          }}/>
-          <label>Ingredient</label>
-          <input type = "text" name = "Ingredient" onChange = {(e) => {
-            setIngredient(e.target.value)
-          }}/>
+          <label className="pt-3">Instruction</label>
+          <input
+            type="text"
+            name="instruction"
+            onChange={(e) => {
+              setInstruction(e.target.value);
+            }}
+          />
+          <label className="pt-3">Ingredient</label>
+          <input
+            type="text"
+            name="Ingredient"
+            onChange={(e) => {
+              setIngredient(e.target.value);
+            }}
+          />
+        </div>
+      </div>
 
-          <button onClick = {submitRecipe}>Submit</button>
-         
-          </div>
-          
+      <button
+        type="button"
+        class="btn btn-primary btn-lg"
+        onClick={submitRecipe}
+      >
+        Submit
+      </button>
     </div>
- 
-  
   );
 }
 
